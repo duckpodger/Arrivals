de9c3ffb5f8c80e94aa9f47ae95e84638e5b77c8
Matthew Amos <matthew.amos@gmail.com>
2017-12-14 13:12:59 +0000

    Final

--- src/test/scala/mamos/ArrivalsTests.scala
+++ src/test/scala/mamos/ArrivalsTests.scala
@@ -1,18 +1,32 @@
 package mamos
 
-import java.time.Clock
-import java.util.concurrent.{ScheduledExecutorService, TimeUnit}
+import java.time.temporal.{ChronoUnit, TemporalUnit}
+import java.time.{Clock, Instant, ZoneId}
+import java.util.concurrent.TimeUnit
 
-import mamos.TflApi.{LineId, StopPointId}
 import org.jmock.lib.concurrent.DeterministicScheduler
 import org.scalatest.mockito.MockitoSugar
 import org.junit.Test
 
 class ArrivalsTests extends MockitoSugar {
-  val scheduler = new DeterministicScheduler
+  // start our test in the 70s why not
+  var currentTime = Instant.ofEpochMilli(0)
 
-  //todo get a clock linked to the scheduler
-  val clock: Clock = ???
+  val scheduler = new DeterministicScheduler {
+    override def tick(duration: Long, timeUnit: TimeUnit): Unit = {
+      currentTime = currentTime.plus(duration, ChronoUnit.MINUTES)
+      super.tick(duration, timeUnit)
+    }
+  }
+
+  //Clock based on our time
+  val clock: Clock = new Clock {
+    override def withZone(zone: ZoneId): Clock = this
+
+    override def getZone: ZoneId = ??? // won't get called
+
+    override def instant(): Instant = currentTime
+  }
   val arrivalsBoard = mock[ArrivalsBoard]
   val tflApi = mock[TflApi]
 
